a36842dbb1c3f8dddf0916f9386575f76c4402bd
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-04 15:38:01 -0300

    new feature: return -1 if the value is not in list

--- binarySearch.py
+++ binarySearch.py
@@ -1,14 +1,34 @@
+def lowerValue(array):
+    lowerValue = array[0] 
+    for i in range(0, len(array)):
+        if array[i] < lowerValue:
+            lowerValue = array[i]
+    return lowerValue
+
+def ordenedList(array):
+    oldList = array
+    newList = []
+    for i in range(0, len(array)):
+        value = lowerValue(oldList)
+        newList.append(value)
+        oldList.remove(value)
+    return newList
+
+
 def binarySearch(array, value):
-    start = 0
-    end = len(array) - 1
+    array = ordenedList(array)
+    start, end = 0, len(array) - 1
+    if value < array[start] or value > array[end]:
+        return - 1
     while True:
-        middle = round((start + end)/ 2)
-        if (array[middle] < value):
-            start = middle
-        elif (array[middle] > value):
+        middle = round((start + end)/2)
+        if value < array[middle]:
             end = middle
+        elif value > array[middle]:
+            start = middle
         else:
             return middle
-
-
-print(binarySearch([5, 10, 15, 20, 25, 30, 35, 40], 20))
+        if value < array[middle]: 
+            if value > array[start]: 
+                if value != array[middle - 1]:
+                    return - 1
